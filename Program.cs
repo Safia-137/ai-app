135d32eed964256f941d0ecb5bec9366fedd0a1f
mohammed jameel <safiaanjum1703@gmail.com>
2026-04-19 02:00:33 +0530

    Initial commit of AI application

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Application started");
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Processing request {i} at {DateTime.Now}");
+            System.Threading.Thread.Sleep(2000); // simulate work
+        }
+
+        Console.WriteLine("Application finished");
+    }
+}
